97f99666721048c9dd48f4625cd70392f58ecb85
imwatsi <imwatsi@gmail.com>
2021-11-10 22:13:27 +0200

    sys status optimizations

--- haf_plug_play/server/system_status.py
+++ haf_plug_play/server/system_status.py
@@ -1,14 +1,20 @@
+from haf_plug_play.database.access import ReadDb
+
+db = ReadDb.db
+
 class SystemStatus:
     sync_status = {}
     
     @classmethod
-    def update_sync_status(cls, sys_status=None, plug_status=None):
-        if sys_status:
-            cls.sync_status['system'] = sys_status
+    def update_sync_status(cls, sync_status=None, plug_status=None):
+        if sync_status:
+            cls.sync_status['sync'] = sync_status
         if plug_status:
             cls.sync_status['plugs'] = plug_status
 
     @classmethod
     def get_sync_status(cls):
+        glob_props = db.get_global_props()
+        cls.sync_status['system'] = glob_props
         return cls.sync_status
 
