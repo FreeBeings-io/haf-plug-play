bae20f799f2a4b9193b7a5493862e238cc92b8c6
imwatsi <imwatsi@gmail.com>
2021-11-11 19:23:08 +0200

    status fix

--- haf_plug_play/server/system_status.py
+++ haf_plug_play/server/system_status.py
@@ -1,7 +1,8 @@
+from os import stat, system
 from haf_plug_play.database.access import ReadDb
 from haf_plug_play.server.normalize import normalize_types
 
-db = ReadDb.db
+db = ReadDb().db
 
 class SystemStatus:
     sync_status = {}
@@ -18,3 +19,11 @@ class SystemStatus:
         glob_props = normalize_types(db.get_global_props())
         cls.sync_status['system'] = glob_props
         return cls.sync_status
+
+    @classmethod
+    def get_latest_block(cls):
+        status = cls.get_sync_status()
+        if 'system' in status:
+            if 'head_block_num' in status:
+                return status['system']['head_block_num']
+        return None
