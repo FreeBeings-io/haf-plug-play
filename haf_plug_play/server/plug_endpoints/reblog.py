7e11f0ddd7c77f44aebd2d888877e78f57e05a95
imwatsi <imwatsi@gmail.com>
2021-11-23 16:02:20 +0200

    add reblogops  endpoint

--- haf_plug_play/server/plug_endpoints/reblog.py
+++ haf_plug_play/server/plug_endpoints/reblog.py
@@ -7,6 +7,23 @@ from haf_plug_play.server.normalize import populate_by_schema
 
 db = ReadDb().db
 
+@method(name="plug_play_api.reblog.get_reblog_ops")
+async def get_reblog_ops(account=None, author=None,  block_range=None):
+    sql = SearchOps.reblog(
+        reblog_account=account,
+        blog_author=author,
+        block_range=block_range
+    )
+    result = []
+    if sql:
+        res = db.db.select(sql) or []
+        for entry in res:
+            result.append(populate_by_schema(
+                entry, ['transaction_id', 'acc_auths', 'account', 'author', 'permlink']
+            ))
+
+    return Success(result)
+
 @method(name="plug_play_api.reblog.get_account_reblogs")
 async def get_follow_ops(account) -> Result:
     """Returns a list of reblogs that an account has made."""
