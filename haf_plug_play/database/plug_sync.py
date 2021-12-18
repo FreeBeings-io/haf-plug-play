dc87410a7a5080149594aa53a2f12abc426af938
imwatsi <imwatsi@gmail.com>
2021-12-18 03:43:44 +0200

    fix sync status output

--- haf_plug_play/database/plug_sync.py
+++ haf_plug_play/database/plug_sync.py
@@ -84,7 +84,7 @@ class PlugSync:
                 elif (head_hive_rowid - app_hive_rowid) > 0:
                     print(f"POLLS:: processing {app_hive_rowid+1} to {head_hive_rowid}")
                     progress = round((app_hive_rowid/head_hive_rowid) * 100, 2)
-                    cls.plug_sync_states['polls'] = f'synchronizing ({progress} %'
+                    cls.plug_sync_states['polls'] = f'synchronizing {progress} %'
                     db.select(f"SELECT public.hpp_polls_update( {app_hive_rowid+1}, {head_hive_rowid} );")
                     db.execute(f"UPDATE public.plug_sync SET latest_hive_rowid = {head_hive_rowid}, state_hive_rowid = {head_hive_rowid} WHERE plug_name='polls';", None)
                     db.commit()
