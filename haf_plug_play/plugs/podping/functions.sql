4d53f52b559e7bd91909a1f3bde3a1baea5c2d18
imwatsi <imwatsi@gmail.com>
2022-02-07 23:15:31 +0200

    fix

--- haf_plug_play/plugs/podping/functions.sql
+++ haf_plug_play/plugs/podping/functions.sql
@@ -79,7 +79,7 @@ CREATE OR REPLACE FUNCTION public.hpp_podping_update( _begin BIGINT, _end BIGINT
                 SELECT pp_podping_opid INTO _new_id FROM _ins;
                 PERFORM hpp_podping_process_op(_new_id, _block_num, _block_timestamp, _required_posting_auths[1], _op_id, _op_payload);
             END LOOP;
-            UPDATE public.plug_sync SET latest_block_num = _block_num, latest_hive_opid = hive_opid, state_hive_opid = hive_opid WHERE plug_name='podping';
+            UPDATE public.plug_sync SET latest_block_num = _block_num, latest_hive_opid = _hive_opid, state_hive_opid = _hive_opid WHERE plug_name='podping';
         END;
         $function$;
 
