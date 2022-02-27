ad8eccc3749874cb04308f5f2d8d00582b7f33e7
imwatsi <imwatsi@gmail.com>
2022-02-27 14:17:40 +0200

    add types and error handling to fastapi

--- haf_plug_play/server/plug_endpoints/podping.py
+++ haf_plug_play/server/plug_endpoints/podping.py
@@ -1,4 +1,5 @@
 """Plug endpoints for podping."""
+from fastapi import HTTPException
 
 from haf_plug_play.database.access import ReadDb
 from haf_plug_play.plugs.podping.podping import SearchQuery, StateQuery
@@ -9,9 +10,11 @@ db = ReadDb().db
 async def get_podping_counts(block_range=None):
     """Returns count summaries for podpings."""
     if block_range:
-        assert isinstance(block_range, list), "Block range must be an array"
-        for x in block_range:
-            assert isinstance(x, int), "Block range items must be integers"
+        if not isinstance(block_range, list):
+            raise HTTPException(status_code=400, detail="Block range must be an array")
+        for block_num in block_range:
+            if not isinstance(block_num, int):
+                raise HTTPException(status_code=400, detail="Block range items must be integers")
     sql = StateQuery.get_podping_counts(block_range)
     result = []
     res = db.db.select(sql) or []
