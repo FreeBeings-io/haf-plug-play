0e47c45d3ec0039d473242d9d7d696aa08b4ab44
imwatsi <imwatsi@gmail.com>
2022-03-04 16:40:22 +0200

    move endpoints

--- haf_plug_play/server/serve.py
+++ haf_plug_play/server/serve.py
@@ -8,7 +8,7 @@ from haf_plug_play.server.plug_endpoints import polls, podping
 from haf_plug_play.server.system_status import SystemStatus
 from haf_plug_play.server.normalize import normalize_types
 
-app = FastAPI()
+app = FastAPI(openapi_url="/api/openapi.json")
 
 async def root():
     """Reports the status of Hive Plug & Play."""
@@ -21,47 +21,47 @@ async def root():
 
 # SYSTEM
 
-app.add_api_route("/", root, tags=["system"], methods=["GET"], summary="System status")
+app.add_api_route("/api", root, tags=["system"], methods=["GET"], summary="System status")
 
 # POLLS
 
 app.add_api_route(
-    "/polls/new_permlink",
+    "/api/polls/new_permlink",
     polls.get_poll_permlink,
     tags=["polls"],
     methods=["POST"],
     summary="A valid and unique permlink to use with a new poll"
 )
 app.add_api_route(
-    "/polls/ops",
+    "/api/polls/ops",
     polls.get_poll_ops,
     tags=["polls"],
     methods=["GET"],
     summary="A list of 'polls' ops within the specified block or time range"
 )
 app.add_api_route(
-    "/polls/active",
+    "/api/polls/active",
     polls.get_polls_active,
     tags=["polls"],
     methods=["GET"],
     summary="A list of current active polls, filterable by tag"
 )
 app.add_api_route(
-    "/polls/{author}/{permlink}",
+    "/api/polls/{author}/{permlink}",
     polls.get_poll,
     tags=["polls"],
     methods=["GET"],
     summary="A poll and its vote details"
 )
 app.add_api_route(
-    "/polls/{author}/{permlink}/votes",
+    "/api/polls/{author}/{permlink}/votes",
     polls.get_poll_votes,
     tags=["polls"],
     methods=["GET"],
     summary="Votes for specified poll"
 )
 app.add_api_route(
-    "/polls/{author}",
+    "/api/polls/{author}",
     polls.get_polls_user,
     tags=["polls"],
     methods=["GET"],
@@ -71,7 +71,7 @@ app.add_api_route(
 # PODPING
 
 app.add_api_route(
-    "/podping/history/counts",
+    "/api/podping/history/counts",
     podping.get_podping_counts,
     tags=["podping"],
     methods=["GET"],
