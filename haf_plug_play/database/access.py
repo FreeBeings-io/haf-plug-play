bb2ca3c5d793bcc506d7d8af6def2c84f84a86a9
imwatsi <imwatsi@gmail.com>
2022-06-07 18:23:34 +0400

    new db access method

--- haf_plug_play/database/access.py
+++ haf_plug_play/database/access.py
@@ -1,10 +1,49 @@
-from haf_plug_play.database.handlers import PlugPlayDb
 from haf_plug_play.database.core import DbSession
+from haf_plug_play.tools import populate_by_schema, normalize_types
 
-class ReadDb:
-    def __init__(self) -> None:
-        self.db = PlugPlayDb()
+_read_db = DbSession()
+_write_db = DbSession()
 
-class WriteDb:
-    def __init__(self) -> None:
-        self.db = DbSession()
+def select(sql:str, schema:list, one:bool = False):
+    _res = _read_db.select(sql)
+    res = []
+    if _res:
+        assert len(schema) == len(_res[0]), 'invalid schema'
+        for x in _res:
+            res.append(populate_by_schema(x,schema))
+        if one:
+            return normalize_types(res)[0]
+        else:
+            return normalize_types(res)
+
+def write(sql:str):
+    try:
+        _write_db.execute(sql, None)
+        _write_db.commit()
+        return True
+    except:
+        return False
+
+def perform(func:str, params:list):
+    #for p in params:
+        #assert isinstance(p, str), 'function params must be strings'
+    string_params = ["%s" for p in params]
+    parameters = ", ".join(string_params)
+    try:
+        _write_db.execute(f"SELECT {func}( {parameters} );", params)
+        _write_db.commit()
+        return True
+    except:
+        return False
+
+def delete(sql:str):
+    try:
+        _write_db.execute(sql)
+        _write_db.commit()
+        return True
+    except:
+        return False
+
+def alter_schema(sql:str):
+    _write_db.execute(sql)
+    _write_db.commit()
