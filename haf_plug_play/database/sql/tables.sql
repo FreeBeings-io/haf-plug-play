007004d4b6b9a61f1b80e98b49daf98661e2b95c
imwatsi <imwatsi@gmail.com>
2022-06-09 08:44:06 +0400

    update core tables

--- haf_plug_play/database/sql/tables.sql
+++ haf_plug_play/database/sql/tables.sql
@@ -8,18 +8,12 @@ CREATE TABLE IF NOT EXISTS hpp.global_props(
     sync_enabled BOOLEAN DEFAULT true
 );
 
-CREATE TABLE IF NOT EXISTS hpp.ops(
-    hpp_op_id BIGSERIAL PRIMARY KEY,
-    hive_opid BIGINT UNIQUE NOT NULL,
-    op_type_id SMALLINT NOT NULL,
-    block_num INTEGER NOT NULL,
-    created TIMESTAMP,
-    transaction_id CHAR(40),
-    body JSON
-) INHERITS( hive.hpp );
-
 CREATE TABLE IF NOT EXISTS hpp.plug_state(
-    module VARCHAR(64) PRIMARY KEY,
+    plug VARCHAR(64) PRIMARY KEY,
     defs JSON,
-    latest_hpp_op_id BIGINT DEFAULT 0
+    latest_block_num BIGINT DEFAULT 0,
+    latest_hive_opid BIGINT DEFAULT 0,
+    sync_enabled BOOLEAN DEFAULT false,
+    run_start BOOLEAN DEFAULT false,
+    run_finish BOOLEAN DEFAULT false
 );
