7097116df2d0cad85e57d239a780bb0f635f53c3
imwatsi <imwatsi@gmail.com>
2022-06-23 11:25:53 +0400

    fix haf init steps

--- haf_plug_play/database/haf.py
+++ haf_plug_play/database/haf.py
@@ -2,7 +2,6 @@ import json
 import os
 import re
 from threading import Thread
-from haf_plug_play.database.access import select, write
 from haf_plug_play.database.core import DbSession
 from haf_plug_play.database.plugs import AvailablePlugs, Plug
 
@@ -22,9 +21,9 @@ class Haf:
 
     @classmethod
     def _check_context(cls, plug, start_block):
-        exists = cls.db.select(
+        exists = cls.db.select_one(
             f"SELECT hive.app_context_exists( '{plug}' );"
-        )[0][0]
+        )
         if exists is False:
             cls.db.select(f"SELECT hive.app_create_context( '{plug}' );")
             cls.db.select(f"SELECT hive.app_context_detach( '{plug}' );")
@@ -34,39 +33,32 @@ class Haf:
     
     @classmethod
     def _check_schema(cls, plug, functions, tables):
-        exists = bool(f"SELECT schema_name FROM information_schema.schemata WHERE schema_name='{plug}';")
-        if not exists:
+        exists = cls.db.select(f"SELECT schema_name FROM information_schema.schemata WHERE schema_name='{plug}';")
+        if exists is None:
             cls.db.execute(tables, None)
             cls.db.execute(functions, None)
+            cls.db.commit()
     
     @classmethod
     def _check_defs(cls, plug, defs):
         _block = defs['props']['start_block'] - 1
-        has = select(f"SELECT plug FROM hpp.plug_state WHERE plug='{plug}'", ['plug'], True)
+        has = cls.db.select_exists(f"SELECT plug FROM hpp.plug_state WHERE plug='{plug}'")
         defs = json.dumps(defs)
-        if has is not None:
-            # update
-            sql = f"""
-                UPDATE hpp.plug_state SET defs='{defs}', latest_block_num={_block} WHERE plug='{plug}';
-            """
-        else:
-            # insert
-            sql = f"""
-                    INSERT INTO hpp.plug_state (plug, hooks, latest_block_num)
+        if has is False:
+            cls.db.execute(
+                f"""
+                    INSERT INTO hpp.plug_state (plug, defs, latest_block_num)
                     VALUES ('{plug}', '{defs}', {_block});
-                """
-        done = write(sql)
-        if done is not True:
-            raise Exception(f"Failed to save defs to DB for plug: '{plug}")
+                """)
 
     @classmethod
     def _init_plugs(cls):
-        cls.plug_list = [f.name for f in os.scandir(dir) if cls._is_valid_plug(f.name)]
+        working_dir = f'{INSTALL_DIR}/plugs'
+        cls.plug_list = [f.name for f in os.scandir(working_dir) if cls._is_valid_plug(f.name)]
         for plug in cls.plug_list:
-            working_dir = f'{INSTALL_DIR}/plugs/{plug}'
-            defs = json.loads(open(f'{working_dir}/defs.json', 'r', encoding='UTF-8').read())
-            functions = open(f'{working_dir}/functions.sql', 'r', encoding='UTF-8').read()
-            tables = open(f'{working_dir}/tables.sql', 'r', encoding='UTF-8').read()
+            defs = json.loads(open(f'{working_dir}/{plug}/defs.json', 'r', encoding='UTF-8').read())
+            functions = open(f'{working_dir}/{plug}/functions.sql', 'r', encoding='UTF-8').read()
+            tables = open(f'{working_dir}/{plug}/tables.sql', 'r', encoding='UTF-8').read()
             cls._check_context(plug, defs['props']['start_block'])
             cls._check_schema(plug, functions, tables)
             cls._check_defs(plug, defs)
@@ -76,8 +68,10 @@ class Haf:
     def _init_hpp(cls):
         tables = open(f'{SOURCE_DIR}/tables.sql', 'r', encoding='UTF-8').read()
         functions = open(f'{SOURCE_DIR}/functions.sql', 'r', encoding='UTF-8').read()
-        cls.db.execute(tables, None)
-        cls.db.execute(functions, None)
+        sync = open(f'{SOURCE_DIR}/sync.sql', 'r', encoding='UTF-8').read()
+        cls.db.execute(tables)
+        cls.db.execute(functions)
+        cls.db.execute(sync)
         cls.db.execute(
             """
                 INSERT INTO hpp.global_props (latest_block_num)
