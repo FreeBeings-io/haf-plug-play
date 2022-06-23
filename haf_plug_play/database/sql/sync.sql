4d3f47aadcf079ffe7269b205cedf4dc03eefedf
imwatsi <imwatsi@gmail.com>
2022-06-23 19:12:04 +0400

    fix main sync script pass to plugs

--- haf_plug_play/database/sql/sync.sql
+++ haf_plug_play/database/sql/sync.sql
@@ -8,14 +8,14 @@ CREATE OR REPLACE FUNCTION hpp.sync_plug(_plug_name VARCHAR(64))
             temprow RECORD;
             _app_context VARCHAR;
             _ops JSON;
+            _op_ids SMALLINT[];
             _next_block_range hive.blocks_range;
-            _head_hive_opid BIGINT;
-            _latest_hive_opid BIGINT;
-            _latest_block_num BIGINT;
+            _latest_block_num INTEGER;
             _range BIGINT[];
         BEGIN
             SELECT defs->'props'->>'context' INTO _app_context FROM hpp.plug_state WHERE plug = _plug_name;
-            SELECT defs->'defs'->'ops' INTO _ops FROM hpp.plug_state WHERE plug = _plug_name;
+            SELECT defs->'ops' INTO _ops FROM hpp.plug_state WHERE plug = _plug_name;
+            SELECT ARRAY (SELECT json_array_elements_text(defs->'op_ids')) INTO _op_ids FROM hpp.plug_state WHERE plug = _plug_name;
 
             IF _app_context IS NULL THEN
                 RAISE NOTICE 'Could not start sync for plug: %. DB entry not found.', _plug_name;
@@ -36,7 +36,7 @@ CREATE OR REPLACE FUNCTION hpp.sync_plug(_plug_name VARCHAR(64))
                     RAISE WARNING 'Waiting for next block...';
                 ELSE
                     RAISE NOTICE 'Attempting to process block range: <%,%>', _next_block_range.first_block, _next_block_range.last_block;
-                    CALL hpp.process_block_range(_plug_name, _app_context, _next_block_range.first_block, _next_block_range.last_block, _ops);
+                    PERFORM hpp.process_block_range(_plug_name, _app_context, _next_block_range.first_block, _next_block_range.last_block, _ops, _op_ids);
                 END IF;
             END LOOP;
             UPDATE hpp.plug_state SET run_start = false WHERE plug = _plug_name;
@@ -44,18 +44,19 @@ CREATE OR REPLACE FUNCTION hpp.sync_plug(_plug_name VARCHAR(64))
         END;
     $function$;
 
-CREATE OR REPLACE PROCEDURE hpp.process_block_range(_plug_name VARCHAR, _app_context VARCHAR, _start BIGINT, _end BIGINT, _ops JSON )
+CREATE OR REPLACE FUNCTION hpp.process_block_range(_plug_name VARCHAR, _app_context VARCHAR, _start INTEGER, _end INTEGER, _ops JSON, _op_ids SMALLINT[] )
+    RETURNS void
     LANGUAGE plpgsql
-    AS
-        $$
+    VOLATILE AS $function$
+
         DECLARE
+            temprow RECORD;
             _plug_schema VARCHAR;
             _done BOOLEAN;
             _massive BOOLEAN;
-            _first_block BIGINT;
-            _last_block BIGINT;
-            _step BIGINT;
-            _op_ids SMALLINT[];
+            _first_block INTEGER;
+            _last_block INTEGER;
+            _step INTEGER;
         BEGIN
             _step := 1000;
             -- determine if massive sync is needed
@@ -67,7 +68,6 @@ CREATE OR REPLACE PROCEDURE hpp.process_block_range(_plug_name VARCHAR, _app_con
             -- get defs
             -- _arr := ARRAY(SELECT json_array_elements_text(_ops));
             -- _op_ids := array_agg(SELECT unnest(_arr[1:999][1]));
-            _op_ids := json_object_keys(_ops);
 
             -- divide range
             FOR _first_block IN _start .. _end BY _step LOOP
@@ -81,35 +81,37 @@ CREATE OR REPLACE PROCEDURE hpp.process_block_range(_plug_name VARCHAR, _app_con
                 -- record run start
                 UPDATE hpp.plug_state SET run_start = true WHERE plug = _plug_name;
                     -- select records and pass records to relevant functions
+                FOR temprow IN
                     EXECUTE FORMAT('
-                        FOR temprow IN
-                            SELECT
-                                %1$sov.id,
-                                %1$sov.op_type_id,
-                                %1$sov.block_num,
-                                %1$sov.timestamp,
-                                %1$sov.trx_in_block,
-                                %1$stv.trx_hash,
-                                %1$sov.body
-                            FROM hive.%1$s_operations_view %1$sov
-                            LEFT JOIN hive.%1$s_transactions_view %1$stv
-                                ON %1$stv.block_num = %1$sov.block_num
-                                AND %1$stv.trx_in_block = %1$sov.trx_in_block
-                            WHERE %1$sov.block_num >= $1
-                                AND %1$sov.block_num <= $2
-                                AND %1$sov.op_type_id = ANY ($1)
-                            ORDER BY %1$sov.block_num, trx_in_block, %1$sov.id
-                        LOOP
-                            PERFORM ( block_num, timestamp, trx_hash, body, $3->op_type_id::varchar );
-                        END LOOP;
-                        ;', _app_context) USING _first_block, _last_block, _op_ids;
+                        SELECT
+                            %1$sov.id,
+                            %1$sov.op_type_id,
+                            %1$sov.block_num,
+                            %1$sov.timestamp,
+                            %1$sov.trx_in_block,
+                            %1$stv.trx_hash,
+                            %1$sov.body::json
+                        FROM hive.%1$s_operations_view %1$sov
+                        LEFT JOIN hive.%1$s_transactions_view %1$stv
+                            ON %1$stv.block_num = %1$sov.block_num
+                            AND %1$stv.trx_in_block = %1$sov.trx_in_block
+                        WHERE %1$sov.block_num >= $1
+                            AND %1$sov.block_num <= $2
+                            AND %1$sov.op_type_id = ANY ($3)
+                        ORDER BY %1$sov.block_num, trx_in_block, %1$sov.id;', _app_context)
+                    USING _first_block, _last_block, _op_ids
+                LOOP
+                    EXECUTE FORMAT('SELECT %s ($1,$2,$3,$4);', (_ops->>(temprow.op_type_id::varchar)))
+                        USING temprow.block_num, temprow.timestamp, temprow.trx_hash, temprow.body;
+                END LOOP;
                 -- save done as run end
-                COMMIT;
                 RAISE NOTICE 'Block range: <%, %> processed successfully.', _first_block, _last_block;
             END LOOP;
             IF _massive = true THEN
                 -- attach context
                 PERFORM hive.app_context_attach(_app_context, _last_block);
             END IF;
+            UPDATE hpp.plug_state SET latest_block_num = _last_block WHERE plug = _plug_name;
+            COMMIT;
         END;
-        $$;
+    $function$;
