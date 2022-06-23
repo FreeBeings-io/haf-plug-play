8fc0a5866e68419aecd47ff4d513f82cb170803c
imwatsi <imwatsi@gmail.com>
2022-06-23 11:27:27 +0400

    adjust hpp schema

--- haf_plug_play/database/sql/tables.sql
+++ haf_plug_play/database/sql/tables.sql
@@ -2,8 +2,6 @@ CREATE SCHEMA IF NOT EXISTS hpp;
 
 CREATE TABLE IF NOT EXISTS hpp.global_props(
     latest_block_num BIGINT DEFAULT 0,
-    latest_hive_rowid BIGINT DEFAULT 0,
-    latest_hpp_op_id BIGINT DEFAULT 0,
     latest_block_time TIMESTAMP,
     sync_enabled BOOLEAN DEFAULT true
 );
@@ -12,7 +10,6 @@ CREATE TABLE IF NOT EXISTS hpp.plug_state(
     plug VARCHAR(64) PRIMARY KEY,
     defs JSON,
     latest_block_num BIGINT DEFAULT 0,
-    latest_hive_opid BIGINT DEFAULT 0,
     run_start BOOLEAN DEFAULT false,
     run_finish BOOLEAN DEFAULT false
 );
