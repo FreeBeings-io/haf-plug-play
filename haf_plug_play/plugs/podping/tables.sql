933da045e95785fd8633e948864b0f930ef59cb0
imwatsi <imwatsi@gmail.com>
2022-06-25 18:41:40 +0400

    fix podping schema

--- haf_plug_play/plugs/podping/tables.sql
+++ haf_plug_play/plugs/podping/tables.sql
@@ -7,17 +7,17 @@ CREATE TABLE IF NOT EXISTS podping.ops(
     trx_id BYTEA NOT NULL,
     req_auths VARCHAR(16)[],
     req_posting_auths VARCHAR(16)[],
-    op_id VARCHAR(31),
+    op_id VARCHAR,
     op_payload JSON
-) INHERITS( hive.plug_play );
+) INHERITS( hive.podping );
 
 CREATE TABLE IF NOT EXISTS podping.feed_updates(
-    feed_update_id BIGSERIAL PRIMARY KEY,
-    pp_podping_opid BIGINT NOT NULL REFERENCES podping.ops(id) ON DELETE CASCADE DEFERRABLE,
+    id BIGSERIAL PRIMARY KEY,
+    podping_id BIGINT NOT NULL REFERENCES podping.ops(id) ON DELETE CASCADE DEFERRABLE,
     block_num INTEGER NOT NULL,
     created TIMESTAMP NOT NULL,
     url VARCHAR(500)
-) INHERITS( hive.plug_play );
+) INHERITS( hive.podping );
 
 
 CREATE INDEX IF NOT EXISTS feed_updates_ix_block_num
