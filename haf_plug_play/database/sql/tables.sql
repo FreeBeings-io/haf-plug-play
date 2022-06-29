cc768587af534b72a07e28ea8ace372f16c660fa
imwatsi <imwatsi@gmail.com>
2022-06-29 16:49:10 +0400

    update schema

--- haf_plug_play/database/sql/tables.sql
+++ haf_plug_play/database/sql/tables.sql
@@ -1,8 +1,7 @@
 CREATE SCHEMA IF NOT EXISTS hpp;
 
 CREATE TABLE IF NOT EXISTS hpp.global_props(
-    latest_block_num BIGINT DEFAULT 0,
-    latest_block_time TIMESTAMP,
+    head_block_num BIGINT DEFAULT 0,
     sync_enabled BOOLEAN DEFAULT true
 );
 
@@ -10,6 +9,5 @@ CREATE TABLE IF NOT EXISTS hpp.plug_state(
     plug VARCHAR(64) PRIMARY KEY,
     defs JSON,
     latest_block_num BIGINT DEFAULT 0,
-    run_start BOOLEAN DEFAULT false,
-    run_finish BOOLEAN DEFAULT false
+    check_in TIMESTAMP
 );
