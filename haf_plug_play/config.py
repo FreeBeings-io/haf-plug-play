26127558d0482b438263bb396a9020c9ab522c8e
imwatsi <imwatsi@freebeings.io>
2022-07-30 20:06:22 +0400

    upgrde config module to docker args

--- haf_plug_play/config.py
+++ haf_plug_play/config.py
@@ -1,13 +1,4 @@
-import json
 import os
-
-HOME_DIR = os.environ.get('PLUG_PLAY_HOME') or "/etc/haf-plug-play"
-
-CONFIG_FIELDS = [
-    'db_host', 'db_name', 'db_username',
-    'db_password', 'server_host', 'server_port'
-]
-
 
 class Config:
     config = {}
@@ -17,32 +8,12 @@ class Config:
         pass
 
     @classmethod
-    def load_config(cls, config_file):
-        values = {}
-        if not os.path.isdir(HOME_DIR):
-            os.mkdir(HOME_DIR)
-        if not os.path.exists(config_file):
-            print(config_file)
-            new_conf = open(config_file, 'w')
-            new_conf.writelines(f"{field}=\n" for field in CONFIG_FIELDS)
-            new_conf.close()
-            print(
-                'No config file detected. A blank one has been created.\n'
-                'Populate it with the correct details and restart haf-plug-play.'
-            )
-            os._exit(1)
-        f = open(config_file, 'r').readlines()
-        for line in f:
-            if '=' in line:
-                setting = line.split('=')
-                _key = setting[0]
-                assert _key in CONFIG_FIELDS, f"invalid config key detected {_key}"
-                _value = setting[1].strip('\n ')
-                if '[' in _value or '{' in _value:
-                    values[_key] = json.loads(_value)
-                else:
-                    values[_key] = _value
-        cls.config = values
-        cls.validate()
+    def load_config(cls):
+        cls.config['db_host'] = os.environ.get('PORT')
+        cls.config['db_name'] = os.environ.get('DB_NAME')
+        cls.config['db_username'] = os.environ.get('DB_USERNAME')
+        cls.config['db_password'] = os.environ.get('DB_PASSWORD')
+        cls.config['server_host'] = os.environ.get('SERVER_HOST')
+        cls.config['server_port'] = os.environ.get('SERVER_PORT')
 
-Config.load_config(HOME_DIR + "/config.ini")
+Config.load_config()
