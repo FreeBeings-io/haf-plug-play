dbfc4e7f6ef146d6811c178bd307a1b8ef208ec2
imwatsi <imwatsi@freebeings.io>
2022-09-26 19:07:05 +0400

    Update tables.sql

--- haf_plug_play/database/sql/tables.sql
+++ haf_plug_play/database/sql/tables.sql
@@ -1,13 +1,13 @@
 CREATE SCHEMA IF NOT EXISTS hpp;
 
 CREATE TABLE IF NOT EXISTS hpp.global_props(
-    head_block_num BIGINT DEFAULT 0,
     sync_enabled BOOLEAN DEFAULT true
 );
 
 CREATE TABLE IF NOT EXISTS hpp.plug_state(
     plug VARCHAR(64) PRIMARY KEY,
     defs JSON,
+    enabled BOOLEAN,
     latest_block_num BIGINT DEFAULT 0,
     latest_block_time TIMESTAMP,
     check_in TIMESTAMP,
