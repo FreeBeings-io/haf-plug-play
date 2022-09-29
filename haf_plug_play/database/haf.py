c4b8c94cc119de9c4b984428b511c3a41967f7c9
imwatsi <imwatsi@freebeings.io>
2022-09-29 19:01:32 +0400

    Update haf.py

--- haf_plug_play/database/haf.py
+++ haf_plug_play/database/haf.py
@@ -10,31 +10,16 @@ from haf_plug_play.tools import INSTALL_DIR
 
 SOURCE_DIR = os.path.dirname(__file__) + "/sql"
 
-MAIN_CONTEXT = "hpp"
-
 config = Config.config
 
 class Haf:
 
-    db = DbSession("hpp-main")
+    db = DbSession(f"{config['schema']}-main")
     plug_list = []
 
     @classmethod
     def _is_valid_plug(cls, module):
         return bool(re.match(r'^[a-z]+[_]*$', module))
-
-    @classmethod
-    def _check_context(cls, name, start_block=None):
-        exists = cls.db.select_one(
-            f"SELECT hive.app_context_exists( '{name}' );"
-        )
-        if exists is False:
-            cls.db.select(f"SELECT hive.app_create_context( '{name}' );")
-            if start_block is not None:
-                cls.db.select(f"SELECT hive.app_context_detach( '{name}' );")
-                cls.db.select(f"SELECT hive.app_context_attach( '{name}', {(start_block-1)} );")
-            cls.db.commit()
-            print(f"HAF SYNC:: created context: '{name}'")
     
     @classmethod
     def _check_schema(cls, plug, tables):
@@ -51,7 +36,7 @@ class Haf:
     @classmethod
     def _check_defs(cls, plug, defs):
         _block = defs['props']['start_block'] - 1
-        has = cls.db.select_exists(f"SELECT plug FROM hpp.plug_state WHERE plug='{plug}'")
+        has = cls.db.select_exists(f"SELECT plug FROM {config['schema']}.plug_state WHERE plug='{plug}'")
         # generate used op type ids array
         _op_ids = []
         for op in defs['ops'].keys():
@@ -61,13 +46,13 @@ class Haf:
         if has is False:
             cls.db.execute(
                 f"""
-                    INSERT INTO hpp.plug_state (plug, defs, latest_block_num, enabled)
+                    INSERT INTO {config['schema']}.plug_state (plug, defs, latest_block_num, enabled)
                     VALUES ('{plug}', '{json.dumps(json.dumps(defs))}', {_block}, {defs['props']['enabled']});
                 """)
         else:
             cls.db.execute(
                 f"""
-                    UPDATE hpp.plug_state SET defs='{json.dumps(defs)}', enabled={defs['props']['enabled']} WHERE plug='{plug}';
+                    UPDATE {config['schema']}.plug_state SET defs='{json.dumps(defs)}', enabled={defs['props']['enabled']} WHERE plug='{plug}';
                 """
             )
         return defs
@@ -88,26 +73,42 @@ class Haf:
 
     @classmethod
     def _init_hpp(cls):
-        cls._check_context(MAIN_CONTEXT)
-        tables = open(f'{SOURCE_DIR}/tables.sql', 'r', encoding='UTF-8').read()
-        functions = open(f'{SOURCE_DIR}/functions.sql', 'r', encoding='UTF-8').read()
-        sync = open(f'{SOURCE_DIR}/sync.sql', 'r', encoding='UTF-8').read()
+        cls.db.execute(f"CREATE SCHEMA IF NOT EXISTS {config['schema']};")
+        tables = open(f'{SOURCE_DIR}/tables.sql', 'r', encoding='UTF-8').read().replace('hpp.', f"{config['schema']}.")
+        functions = open(f'{SOURCE_DIR}/functions.sql', 'r', encoding='UTF-8').read().replace('hpp.', f"{config['schema']}.")
+        sync = open(f'{SOURCE_DIR}/sync.sql', 'r', encoding='UTF-8').read().replace('hpp.', f"{config['schema']}.")
         cls.db.execute(tables)
         cls.db.execute(functions)
         cls.db.execute(sync)
         cls.db.execute(
-            """
-                INSERT INTO hpp.global_props (sync_enabled)
+            f"""
+                INSERT INTO {config['schema']}.global_props (sync_enabled)
                 SELECT 'true'
-                WHERE NOT EXISTS (SELECT * FROM hpp.global_props);
+                WHERE NOT EXISTS (SELECT * FROM {config['schema']}.global_props);
             """, None
         )
         cls.db.commit()
 
     @classmethod
+    def _cleanup(cls):
+        """Stops any running sync procedures from previous instances."""
+        running = cls.db.select_one(f"SELECT {config['schema']}.is_sync_running('{config['schema']}-main');")
+        if running is True:
+            cls.db.execute(f"SELECT {config['schema']}.terminate_main_sync('{config['schema']}-main');")
+        cmds = [
+            f"DROP SCHEMA {config['schema']} CASCADE;",
+        ]
+        if config['reset'] == 'true':
+            for cmd in cmds:
+                try:
+                    cls.db.execute(cmd)
+                except Exception as err:
+                    print(f"Reset encountered error: {err}")
+
+    @classmethod
     def _start_sync(cls):
         print("Starting main sync process...")
-        cls.db.execute(f"CALL hpp.sync_main();")
+        cls.db.execute(f"CALL {config['schema']}.sync_main();")
 
     @classmethod
     def init(cls):
