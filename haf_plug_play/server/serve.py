885dbc20ba8fd47c03ee87b508faf0ed06c072dc
imwatsi <imwatsi@freebeings.io>
2022-10-15 17:46:13 +0400

    disable reload api

--- haf_plug_play/server/serve.py
+++ haf_plug_play/server/serve.py
@@ -58,6 +58,6 @@ def run_server():
         host=config['server_host'],
         port=int(config['server_port']),
         log_level="info",
-        reload=True,
+        reload=False,
         workers=int(config['server_workers'])
     )
