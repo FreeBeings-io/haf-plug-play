2d6d8b99917e5976124bbe364a63ecdba28f807f
imwatsi <imwatsi@freebeings.io>
2023-01-26 14:19:43 +0200

    fix endpoint

--- haf_plug_play/server/plug_endpoints/deleg.py
+++ haf_plug_play/server/plug_endpoints/deleg.py
@@ -15,7 +15,7 @@ async def get_acc_bals(account:str):
     if len(account) > 16:
         raise HTTPException(status_code=400, detail="Hive account must be no more than 16 chars")
     sql = StateQuery.get_deleg_account_bals(account)
-    res = select(sql, ['balances'])
+    res = select(sql, ['balances'])[0]
     result = {
         'account': account,
         'balances': res['balances']
