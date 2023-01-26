3842ca2a1fa10af62c0c5d5d35db28dd7450ef41
imwatsi <imwatsi@freebeings.io>
2023-01-26 14:24:01 +0200

    refactor

--- haf_plug_play/plugs/deleg/functions.sql
+++ haf_plug_play/plugs/deleg/functions.sql
@@ -32,8 +32,8 @@ CREATE OR REPLACE FUNCTION deleg.get_acc_bals(_acc VARCHAR(16))
             WHERE delegator = _acc;
             
             RETURN jsonb_build_object(
-                'received',round((_received::numeric)/1000000, 6),
-                'given',round((_given::numeric)/1000000, 6)
+                'in',round((_received::numeric)/1000000, 6),
+                'out',round((_given::numeric)/1000000, 6)
             );
         END;
     $function$;
