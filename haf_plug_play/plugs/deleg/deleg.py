31fe26322207102474a8f0b4e74ca1f5fa2675c6
imwatsi <imwatsi@freebeings.io>
2023-01-31 17:09:42 +0200

    add current delegation endpoints

--- haf_plug_play/plugs/deleg/deleg.py
+++ haf_plug_play/plugs/deleg/deleg.py
@@ -17,3 +17,23 @@ class StateQuery:
             SELECT deleg.get_acc_bals('{account}')
         """
         return schemafy(query, 'deleg')
+
+    @classmethod
+    def get_deleg_in(cls, account:str):
+        query = f"""
+            SELECT id, created, delegator, round((amount::numeric)/1000000, 6)
+            FROM deleg.delegations_vesting
+            WHERE amount > 0 AND delegatee = '{account}'
+            ORDER BY id ASC
+        """
+        return schemafy(query, 'deleg')
+    
+    @classmethod
+    def get_deleg_out(cls, account:str):
+        query = f"""
+            SELECT id, created, delegatee, round((amount::numeric)/1000000, 6)
+            FROM deleg.delegations_vesting
+            WHERE amount > 0 AND delegator = '{account}'
+            ORDER BY id ASC
+        """
+        return schemafy(query, 'deleg')
--- haf_plug_play/server/plug_endpoints/deleg.py
+++ haf_plug_play/server/plug_endpoints/deleg.py
@@ -7,6 +7,9 @@ from haf_plug_play.plugs.deleg.deleg import SearchQuery, StateQuery
 from haf_plug_play.database.access import select
 from haf_plug_play.tools import UTC_TIMESTAMP_FORMAT
 
+SCHEMA_DELEG_IN = ['id', 'created', 'delegator', 'amount']
+SCHEMA_DELEG_IN = ['id', 'created', 'delegatee', 'amount']
+
 router_deleg = APIRouter()
 
 @router_deleg.get("/api/deleg/account", tags=['deleg'])
@@ -21,3 +24,21 @@ async def get_acc_bals(account:str):
         'balances': res['balances']
     }
     return result
+
+@router_deleg.get('/api/deleg/in', tags=['deleg'])
+async def get_acc_in(account:str):
+    """Returns current delegations made to given account."""
+    if len(account) > 16:
+        raise HTTPException(status_code=400, detail="Hive account must be no more than 16 chars")
+    sql = StateQuery.get_deleg_in(account)
+    res = select(sql, SCHEMA_DELEG_IN)[0]
+    return res
+
+@router_deleg.get('/api/deleg/out', tags=['deleg'])
+async def get_acc_out(account:str):
+    """Returns current delegations made from a given account."""
+    if len(account) > 16:
+        raise HTTPException(status_code=400, detail="Hive account must be no more than 16 chars")
+    sql = StateQuery.get_deleg_in(account)
+    res = select(sql, SCHEMA_DELEG_IN)[0]
+    return res
